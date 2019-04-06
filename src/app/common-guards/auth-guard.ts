c7b05828efda87b79c4913d56756c48288cc051f
OleksiiY <nonse77@yandex.ua>
2019-04-06 10:16:04 +0300

    minor change #2

--- src/app/common-guards/auth-guard.ts
+++ src/app/common-guards/auth-guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import {GlobalAuthService} from '../common-services/global-auth.service';
+import { GlobalAuthService } from '../common-services/global-auth.service';
 
 @Injectable({
   providedIn: 'root'
