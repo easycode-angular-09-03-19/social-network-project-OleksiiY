e4675ef133fe20112d6ef02ab1ac4bedb5bc511c
OleksiiY <nonse77@yandex.ua>
2019-04-16 22:47:01 +0300

    Aleksey Baliuk: added 2 more components with separate notification-item, notifications components

--- src/app/modules/navbar/navbar.module.ts
+++ src/app/modules/navbar/navbar.module.ts
@@ -4,12 +4,16 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
+import { NotificationsComponent } from './components/notifications/notifications.component';
+import { NotificationItemComponent } from './components/notification-item/notification-item.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    NotificationsComponent,
+    NotificationItemComponent
   ],
   imports: [
     CommonModule,
